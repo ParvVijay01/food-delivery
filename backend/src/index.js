2fc5dde2122bd46dfc43fd4eb18c8773ad58cb17
Parv vijayvargiya <parvvj2000@gmail.com>
2025-10-04 20:11:25 +0530

    implement user authentication with signup, signin, and signout functionality

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,22 @@
 import express from "express"
 import dotnev from "dotenv"
-import connectDB from "./config/db.js"
 dotnev.config()
+import connectDB from "./config/db.js"
+import cookieParser from "cookie-parser"
+import authRouter from "./routes/auth.routes.js"
+import cors from "cors"
 
 const app = express()
 const PORT = process.env.PORT || 5000
 
-
+//middleware
+app.use(express.json())
+app.use(cookieParser())
+app.use("/api/auth", authRouter)
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials:true,
+}))
 
 app.listen(PORT, (req,res) => {
     connectDB()
